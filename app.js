ad97dc4d6c48a9b504f1d90007dde575097ccc70
CaptainPradip <pradipnemane@gmail.com>
2018-01-22 01:09:40 +0530

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var loginPassport=require('./config/passport')(app,passport);
 
 //import all Schema
 
-var port=process.env.port||8080;
+var port=process.env.port;
 app.use(express.static(__dirname +'/public'));
 // use middleware 
 app.use(bodyParser.urlencoded({extended: false}));
